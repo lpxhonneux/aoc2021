8b10af3045d2d9cd896a9dc7706aebd0380479b1
LPX <lpxhonneux@gmail.com>
2021-12-07 11:27:02 -0500

    Day 5

--- day5/day5.rs
+++ day5/day5.rs
@@ -2,17 +2,39 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+use std::collections::HashMap;
+
+fn draw_line(canvas: &mut HashMap<(i32,i32),i32>, coords: (i32,i32,i32,i32)) {
+    let direction = ((coords.2-coords.0).signum(), (coords.3-coords.1).signum());
+    let mut cur = (coords.0,coords.1);
+    let end = (coords.2, coords.3);
+    let first_coord_counter = canvas.entry(cur).or_insert(0);
+    *first_coord_counter += 1;
+    loop {
+        if cur == end { break; }
+        cur.0 += direction.0;
+        cur.1 += direction.1;
+        let counter = canvas.entry(cur).or_insert(0);
+        *counter += 1;
+    }
+}
+
 fn main() {
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./day5.txt") {
-        let mut line_coords = lines.map(|x| x.unwrap())
+        let line_coords = lines.map(|x| x.unwrap())
                                .map(|x| x.split(&[' ','-','>',','][..])
                                          .filter(|y| !y.is_empty())
-                                         .map(|y| y.parse::<u32>().unwrap())
-                                         .collect::<Vec<u32>>()
+                                         .map(|y| y.parse::<i32>().unwrap())
+                                         .collect::<Vec<i32>>()
                                );
-        println!("Day 5 part i: {:?}", line_coords.nth(0));
-        println!("Day 5 part ii: {}", 0);
+        let (straight, diag):(Vec<(i32,i32,i32,i32)>,Vec<(i32,i32,i32,i32)>) = line_coords.map(|v| (v[0],v[1],v[2],v[3]))
+                                          .partition(|(x0,y0,x1,y1)| x0 == x1 || y0 == y1 );
+        let mut canvas = HashMap::new();
+        for line in straight { draw_line(&mut canvas, line) }
+        println!("Day 5 part i: {:?}", canvas.values().filter(|x| **x > 1).count());
+        for line in diag { draw_line(&mut canvas, line) }
+        println!("Day 5 part ii: {}", canvas.values().filter(|x| ** x > 1).count());
     }
 }
 
