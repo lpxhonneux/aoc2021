d8763dae4d24aa35073640eedc0957a67f4f7ce4
LPX <lpxhonneux@gmail.com>
2021-12-30 10:32:47 -0500

    Day 10

--- day10/day10.rs
+++ day10/day10.rs
@@ -12,20 +12,40 @@ pub enum Delim {
 fn main() {
     // File hosts must exist in current path before this produces output
     if let Ok(lines) = read_lines("./day10.txt") {
-        let mut corrupt_lines = lines.map(|x| x.unwrap()
+        let all_lines = lines.map(|x| x.unwrap()
                                                 .chars()
-                                                .fold((Vec::new(), false), |acc, y|  {
+                                                .fold((Vec::new(), false), |mut acc, y|  {
                                                     if !acc.1 {
-                                                        let corrupt = match *y {
-                                                            '(' => {acc.0.push(*y); false},
-                                                            '{' => {acc.0.push(*y); false},
-                                                            '<' => {acc.0.push(*y); false},
-                                                            '[' => {acc.0.push(*y); false},
-                                                            y  => {if let Some(c) = acc.0.pop(); {
-                                                                c == y
-                                                            } else { true }
+                                                        let corrupt = match y {
+                                                            '(' => {acc.0.push(y); false},
+                                                            '{' => {acc.0.push(y); false},
+                                                            '<' => {acc.0.push(y); false},
+                                                            '[' => {acc.0.push(y); false},
+                                                            '}'  => {
+                                                                if let Some(c) = acc.0.pop() {
+                                                                    c != '{'
+                                                                } else { true }
                                                             },
+                                                            ')'  => {
+                                                                if let Some(c) = acc.0.pop() {
+                                                                    c != '('
+                                                                } else { true }
+                                                            },
+                                                            ']'  => {
+                                                                if let Some(c) = acc.0.pop() {
+                                                                    c != '['
+                                                                } else { true }
+                                                            },
+                                                            '>'  => {
+                                                                if let Some(c) = acc.0.pop() {
+                                                                    c != '<'
+                                                                } else { true }
+                                                            },
+                                                            _    => false,
                                                         };
+                                                        if corrupt {
+                                                            acc.0.push(y);
+                                                        }
                                                         (acc.0, corrupt)
                                                     } else {
                                                         acc
@@ -33,8 +53,39 @@ fn main() {
                                                 }
                                                 )
                                           );
-        println!("Day 10 part i: {}", 0);
-        println!("Day 10 part ii: {:?}", 0);
+        let (uncorrupted_lines, corrupt_lines) : (Vec<(Vec<char>, bool)>,Vec<(Vec<char>, bool)>) = all_lines.partition(|x| !x.1);
+        println!("Day 10 part i: {}", corrupt_lines.into_iter().fold(0, |acc, mut x| {
+                                              if x.1 {
+                                                  if let Some(c) = x.0.pop() {
+                                                      match c {
+                                                          ')' => acc+3,
+                                                          ']' => acc+57,
+                                                          '}' => acc+1197,
+                                                          '>' => acc+25137,
+                                                          _   => acc+0,
+                                                      }
+                                                  } else {
+                                                      acc
+                                                  }
+                                              } else {
+                                                  acc
+                                              }
+                                          }));
+        let mut missing_score = uncorrupted_lines.into_iter()
+            .map(|x| x.0)
+            .map(|x| {
+                x.iter().rev().fold(0 as u64, |acc, y| {
+                    match y {
+                        '(' => {acc*5+1},
+                        '{' => {acc*5+3},
+                        '<' => {acc*5+4},
+                        '[' => {acc*5+2},
+                        _   => {acc},
+                    }
+                })
+            }).collect::<Vec<u64>>();
+        missing_score.sort();
+        println!("Day 10 part ii: {:?}", missing_score[missing_score.len()/2]);
     }
 }
 
