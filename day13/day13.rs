48b5995fd77101a17dc54ebba93c4fdfb2482214
LPX <lpxhonneux@gmail.com>
2022-01-01 09:07:32 -0500

    Day 13 A bit more adhoc than I would have liked.

--- day13/day13.rs
+++ day13/day13.rs
@@ -1,44 +1,63 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::cmp::max;
 
 use std::collections::HashSet;
 
-pub fn hash(val: usize, size: usize) -> usize {
-    val % size
-}
-
-pub fn coalesce(indices: Vec<(usize,usize)>, size: usize) -> Vec<(usize,usize)> {
-    let mut hashset = vec![(false,0,0); indices.len()];
-    indices.iter().map(|x| { hashset[hash(x.0 * size + x.1, size)] = (true, x.0, x.1); });
-    hashset.iter().filter_map(|x| if x.0 { Some((x.1,x.2)) } else { None }).collect::<Vec<(usize,usize)>>()
+pub fn coalesce(indices: Vec<(usize,usize)>) -> Vec<(usize,usize)> {
+    let mut set_idx = HashSet::new();
+    indices.iter().for_each(|x| { set_idx.insert((x.0, x.1)); });
+    set_idx.drain().collect::<Vec<(usize,usize)>>()
 }
 
 pub fn flip(indices: (usize, usize), axis: char, value: usize) -> (usize, usize) {
     match axis {
-        'x' => { (indices.0, if indices.1 > value { 2*value - indices.1 } else { indices.1 }) },
-        _   => { (if indices.0 > value { 2*value - indices.0 } else { indices.0 }, indices.0) },
+        'y' => { (indices.0, if indices.1 >= value { 2*value - indices.1 } else { indices.1 }) },
+        _   => { (if indices.0 >= value { 2*value - indices.0 } else { indices.0 }, indices.1) },
+    }
+}
+
+pub fn visualise(indices: Vec<(usize,usize)>) {
+    let (max_x, max_y) = indices.iter().fold((0,0), |acc, (x,y)| (max(acc.0,*x),max(acc.1,*y)) );
+    let mut canvas = vec![vec!['.';max_x+1];max_y+1];
+    for (i,j) in indices {
+        canvas[j][i] = '#';
+    }
+    for line in canvas {
+        let s : String = line.iter().collect();
+        println!("{}", s);
     }
 }
 
 fn main() {
     // File hosts must exist in current path before this produces output
     if let Ok(mut lines) = read_lines("./day13.txt") {
-        let mut map = lines.take_while(|x| !x.unwrap().as_ref().is_empty())
+        let map = lines.by_ref()
+                           .take_while(|x:&Result<String, std::io::Error>| !x.as_ref().unwrap().is_empty())
                            .map(|x| x.unwrap())
                            .map(|x| {
-                                let mut idx = x.split(' ');
+                                let mut idx = x.split(',');
                                 (idx.nth(0).unwrap().parse::<usize>().unwrap(),
                                  idx.nth(0).unwrap().parse::<usize>().unwrap())
                            })
                            .collect::<Vec<(usize,usize)>>();
-        let mut final_map = lines.map(|x| x.unwrap()).map(|x| x.split(' ').nth(2).unwrap()).map(|x| {
-            let mut line = x.split('=');
-            (line.nth(0).unwrap().chars().nth(0).unwrap(), line.nth(0).unwrap().parse::<usize>().unwrap())
-        }).fold(map, |acc, x| acc.iter().map(|y| flip(*y, x.0, x.1)).collect::<Vec<(usize,usize)>>());
-        final_map = coalesce(final_map, 1500);
-        println!("Day 13 part i: {}", final_map.len());
-        println!("Day 13 part ii: {:?}", 0);
+        let mut final_map = lines.map(|x| x.unwrap())
+                                 .map(|x| {
+                                     let mut s = x.split(' ');
+                                     let t = s.nth(2).unwrap();
+                                     let mut line = t.split('=');
+                                    (line.nth(0).unwrap().chars().nth(0).unwrap(),
+                                     line.nth(0).unwrap().parse::<usize>().unwrap())
+                                    }
+                                 ).fold(map,
+                                        |acc, x| acc.iter().map(|y| flip(*y, x.0, x.1)).collect::<Vec<(usize,usize)>>());
+        final_map = coalesce(final_map);
+        // Getting the solutions is a bit adhoc for part 1 simply remove the all the unnecessary fold instructions from the file
+        // For part 2 we print it our to screen and then visually recognize the code
+        println!("Day 13 part number of elements: {}", final_map.len());
+        println!("Day 13 part visual");
+        visualise(final_map);
     }
 }
 
